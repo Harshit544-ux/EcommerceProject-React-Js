9ebeabb95d58f26227eee30f878cba00cce58eba
Harshit <srivastava0798@gmail.com>
2025-07-29 13:14:29 +0530

    add productitem component and integrate it into latest collection

--- frontend/src/component/LatestCollection.jsx
+++ frontend/src/component/LatestCollection.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
+import ProductItem from './ProductItem';
 
 function LatestCollection() {
     const { products } = useContext(ShopContext)
     const [latestProducts,setLatestProducts]=useState([]);
-
 
     useEffect(()=>{
         setLatestProducts(products.slice(0,10));
@@ -17,8 +17,14 @@ function LatestCollection() {
             <Title text1={'LATEST'} text2={'COLLECTIONS'} />
             <p className='w-3/4 mx-auto text-xs sm:text-sm md:text-base text-gray-600'>Unlock the latest collections, featuring the best products to enhance your everyday experience.</p>
         </div>
+         {/* {Rendering product} */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 mt-4 px-10 py-6'>
+        {latestProducts.map((item,index) => (
+          <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price} />
+        ))}
+      </div>
     </div>
   )
-}
+} 
 
 export default LatestCollection
