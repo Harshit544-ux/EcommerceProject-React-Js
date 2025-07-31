51dd626f522dac104ae0f2ddf8dc4c852e281c88
Harshit <srivastava0798@gmail.com>
2025-07-31 17:16:21 +0530

    ajsdhfjkha

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import 'dotenv/config'
+import supabase from './config/supabase.js'
 
 // App Config
 const app = express();
@@ -13,6 +14,15 @@ app.use(cors());
 // API Endpoints
 app.get('/', (req, res) => {
   res.send("API is working");
+});
+
+app.get('/test-supabase', async (req, res) => {
+  const { data, error } = await supabase.from('pg_tables').select('tablename').eq('schemaname', 'public');
+  if (error) {
+    res.status(500).json({ error: error.message });
+  } else {
+    res.status(200).json({ tables: data });
+  }
 });
 
 // Start server
